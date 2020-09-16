a3091ec7441c0d1d58484c7ffe423d5506e02795
ahmedfathy <ahmedfathyr44@gmail.com>
2020-09-16 15:23:55 -0400

    Make last version of FoxMart with some UI uodates

--- app/src/main/java/com/devahmed/techx/mercadomart/Common/MVC/MvcView.java
+++
@@ -1,9 +0,0 @@
-package com.devahmed.techx.mercadomart.Common.MVC;
-
-import android.view.View;
-
-public interface MvcView {
-
-    View getRootView();
-
-}
--- app/src/main/java/com/devahmed/techx/mercadomart/Screens/Settings/SettingsMvc.java
+++
@@ -1,8 +0,0 @@
-package com.devahmed.techx.mercadomart.Screens.Settings;
-
-public interface SettingsMvc {
-    interface Listener{
-        void onChangLanguageBtnClicked(boolean isChecked);
-    }
-    void bindData(String language);
-}
